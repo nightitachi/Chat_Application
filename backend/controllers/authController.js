cd2bb79e930e3aafcd79568b9abebe2e63d7af71
Belhrak Ali <140948858+nightitachi@users.noreply.github.com>
2024-11-22 15:02:42 +0100

    ejdid ejdid ya reba7

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import User from '../models/authmodel.js';
-import bcrypt from 'bcryptjs';
+import React from "react";
+import User from "../models/authmodel.js";
+import bcrypt from "bcryptjs";
 
 // Signup functionality
 export const signup = async (req, res) => {
   try {
-    const { fullName, username, password, ConfirmedPassword, gender } = req.body;
+    const { fullName, username, password, ConfirmedPassword, gender } =
+      req.body;
 
     if (password.length < 6) {
       return res.status(400).json("Password must be more than 6 characters");
@@ -39,7 +40,7 @@ export const signup = async (req, res) => {
       profilePic: "",
     });
   } catch (error) {
-    console.error("Signup error:", error); 
+    console.error("Signup error:", error);
     res.status(500).json("Internal server error");
   }
 };
@@ -47,14 +48,33 @@ export const signup = async (req, res) => {
 // Login functionality
 export const login = async (req, res) => {
   try {
-    const {username , password } = req.body;
-    
-    const user = await User.findOne({username})
-    const ispasswordCorrect = bcrypt.compare(password)
+    const { username, password } = req.body;
 
+    // Check if the username exists
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(404).json({ message: "Wrong username!" });
+    }
+
+    // Compare the password with the hashed password
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (!isPasswordCorrect) {
+      return res.status(401).json({ message: "Wrong password!" });
+    }
+
+    // Send user data in response
+    const { _id, fullName, profilePic } = user;
+    res.status(200).json({
+      _id,
+      fullName,
+      username,
+      profilePic,
+    });
   } catch (error) {
-    console.error("Signup error:", error); 
-    res.status(500).json("Internal server error");
+    console.error(error);
+    res
+      .status(500)
+      .json({ message: "Something went wrong. Please try again." });
   }
 };
 
